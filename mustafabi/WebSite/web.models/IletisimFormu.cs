bb35daca2cc7a9697810d225921fa0cd7728955f
Ceren Sinen <ceren.sinen@hotmail.com>
2026-04-14 13:42:09 +0300

    Delete mustafabi directory

--- mustafabi/WebSite/web.models/IletisimFormu.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProjeninAdi.Models // ProjeninAdi kısmına kendi proje adını yaz
-{
-    public class IletisimFormu
-    {
-        public string AdSoyad { get; set; }
-        public string Email { get; set; }
-        public string Mesaj { get; set; }
-    }
-}
